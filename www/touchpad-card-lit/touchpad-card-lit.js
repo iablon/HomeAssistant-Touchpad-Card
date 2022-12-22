b79e5e59fe7a902028a8ef58687074712e3dd72e
Hackerino007 <mysotto8@gmail.com>
2022-12-22 20:39:20 +0100

    edit tabbedd menu(gui only)

--- www/touchpad-card-lit/touchpad-card-lit.js
+++ www/touchpad-card-lit/touchpad-card-lit.js
@@ -1,11 +1,17 @@
 import { html, LitElement,css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+import {classMap} from './node_modules/lit/directives/class-map.js';
+import {styleMap} from './node_modules/lit/directives/style-map.js';
 
 const vibrate = new Event('haptic', {bubbles: false});
 const moreInfo = new Event('hass-more-info', { composed: true });
 
 var clickTimer,holdTimer,holdInterval,falseCheck = true;
-
 let bool=false;
+const editTabs = {
+  power: true,
+  source: false,
+  
+}
 const buttons = [
   {txt: 'power',icon: 'mdi:power',class: 'power',call_data: {domain: 'homeassistant',service: 'toggle'}},
   {txt: 'chup',repeatOnHold: true,icon: 'mdi:arrow-up-bold-circle',class: 'ch-plus',call_data: {domain: 'media_player',service: 'play_media'},key_data: {media_content_id: 'KEY_CHUP', media_content_type: 'send_key'}},
@@ -30,7 +36,7 @@ class MyElement extends LitElement {
             <the-tv class="${this.config.fancy_borders ? 'fancy-borders' : ''}" style="height:${window.navigator.userAgent.includes("Home Assistant") ? '100vh' : window.navigator.brave != undefined ? '80vh' : '85vh' };">
               <div id="entity-area" @dblclick="${()=>this.moreInfoAction(this)}">
                 ${this.tvIconOrSource()}
-                <b>${ this.config.name != undefined ? this.config.name : this.hass.states[this.config.entity].attributes.friendly_name.toUpperCase()}</b>
+                <b >${ this.config.name }</b>
                 ${this.makeMeButton(0)}
               <hr>
               </div>
@@ -100,6 +106,7 @@ class MyElement extends LitElement {
         }
 
         #buttons{
+          
             display: grid;
             grid-template-columns: auto auto auto; 
             grid-template-rows: auto auto; 
@@ -205,6 +212,7 @@ class MyElement extends LitElement {
 static getConfigElement() {
   return document.createElement("content-card-editor");
 }
+
 
 tvIconOrSource(ha = this.hass){
   if(ha.states[this.config.entity].attributes.entity_picture != undefined)
@@ -326,6 +334,10 @@ else {
       clearTimeout(clickTimer);
       clearTimeout(holdTimer)
     });
+
+    setTimeout(() => {
+      window.scrollTo(0,document.body.scrollHeight);
+    }, 1000);
     return button;
 }
 }
@@ -333,7 +345,7 @@ execute(i){
   let context = buttons[i >= 7 ? 7 : i];
   if(context.call_data.key_data != undefined)
     if(i == 8)
-      this.hass.callService(buttons.call_data.domain,context.call_data.service,{media_content_id: 'KEY_ENTER', media_content_type: context.call_data.key_data.media_content_type},{entity_id: this.config.entity});
+      this.hass.callService(context.call_data.domain,context.call_data.service,{media_content_id: 'KEY_ENTER', media_content_type: context.call_data.key_data.media_content_type},{entity_id: this.config.entity});
     else if(i == 9)
       this.hass.callService(context.call_data.domain,context.call_data.service,{media_content_id: 'KEY_RETURN', media_content_type: context.call_data.key_data.media_content_type},{entity_id: this.config.entity});
     else if(i == 10)
@@ -402,8 +414,6 @@ setConfig(config) {
     this.config = config;
 }
 }
-
-
 class ContentCardEditor extends LitElement {
 
   static get properties() {
@@ -412,44 +422,165 @@ class ContentCardEditor extends LitElement {
       _config: {},
     };
   }
+  constructor (){
+    super();
+    this.updateIt();
+    this.tabs = {power: false,source: false,mute:false,touchpad: false,settings:true,dblclick: false,hold: false};
+    this.targ = 'settings' ;
+
+  }
+
+  static properties = {
+    select: {state: true},
+    tabs: {state: true},
+  };
 
   setConfig(config) {
     this._config = config;
+    console.log(this._config);
   }
 
     render(){
       return html`
-          <ha-select id="entity-selector" naturalMenuWidth fixedMenuPosition label="Entità" @selected="${this.updateIt}" @closed="${ev => ev.stopPropagation()}"  >
+          <ha-select id="entity-selector"  .value="${this._config?.entity }"  label="Entità" @selected="${this.updateIt}" @closed="${ev => ev.stopPropagation()}"  >
           ${Object.keys(this.hass.states).filter(ent => ent.match('media_player[.]')).map(entity => {
                             return html` <mwc-list-item .value="${entity}">${entity}</mwc-list-item> `;
                         })}
-          </ha-select><br>
-          <ha-formfield class="switch-wrapper"  label="Fancy Borders">
-          <ha-switch id="fancy-borders-selector" .checked="${this._config.fancy_borders}" @change="${this.updateIt}"></ha-switch>
-        </ha-formfield><br>
-        
+          </ha-select>
+        ${this._config?.entity != undefined ? this.makeMeTabMenu() : null}
       `;
     }  
 
-    oneTab(){
-      return html`
 
+    makeMeTabMenu(){
+      return html`
+        <tabbed-menu-container >
+          <div class="tab-row" >
+            <div class="power tab ${this.tabs.power ? 'selected' : ''}" @click="${this.changeTab}"><ha-icon icon="mdi:power"></ha-icon></div>
+            <div class="source tab ${this.tabs.source ? 'selected' : ''}" @click="${this.changeTab}"><ha-icon icon="mdi:logout-variant"></ha-icon></div>
+            <div class="mute tab ${this.tabs.mute ? 'selected' : ''}" @click="${this.changeTab}"><ha-icon icon="mdi:volume-mute"></ha-icon></div>
+            <div class="touchpad tab ${this.tabs.touchpad ? 'selected' : ''}" @click="${this.changeTab}"><ha-icon icon="mdi:trackpad"></ha-icon></div>
+            <div class="settings tab ${this.tabs.settings ? 'selected' : ''}" @click="${this.changeTab}"><ha-icon icon="mdi:tune"></ha-icon></div>
+          </div>
+          <div class="sub-tabs">
+          ${this.subTabs()}
+        </div>
+      </tabbed-menu-container>
       `;
     }
 
+  subTabs(){
+    return html`
+          <div class="sub-tabs-row ${this.tabs.settings ? 'hide' : 'show'}" >
+            <div class="dblclick tab ${this.tabs.dblclick ? 'sub-selected' : ''}" @click="${this.changeTab}">Double click</div>
+            <div class="hold tab ${this.tabs.hold ? 'sub-selected' : ''}" @click="${this.changeTab}">Hold</div>
+          </div>
+          <div class="content">
+            ${this.putUpConfig()}
+          </div>
+    `;
+  }
+
+  putUpConfig(){
+    let currSelection = Object.keys(this.tabs).filter(e => this.tabs[e] === true);
+    if(currSelection[0] === 'settings')
+      return this.generalConfig();
+    else
+      switch(currSelection[1]){
+        case 'dblclick':
+          return this.dblclickConfig(currSelection[0]);
+        case 'hold':
+          return this.holdConfig(currSelection[0]);
+      }
+  
+  }
+
+  dblclickConfig(a){
+    return html`
+    <ha-select id="dblclick-service-selector"  .value="${ this._config.options[a].dblclick }" label="Double click action" @selected="${this.updateIt }" @closed="${ev => ev.stopPropagation()}"  >
+      <mwc-list-item .value="${'no-action'}">No action</mwc-list-item>
+      ${Object.keys(this.hass.services.script).map(script => {
+                        return html` <mwc-list-item .value="${script}">script.${script}</mwc-list-item> `;
+                    })}
+      </ha-select>`;
+  }
+  holdConfig(a){
+    return html`
+    <ha-select id="hold-service-selector" id="name" .value="${ this._config.options[a].hold}" label="Hold action" @selected="${ this.updateIt }" @closed="${ev => ev.stopPropagation()}"  >
+      <mwc-list-item .value="${'no-action'}">No action</mwc-list-item>
+      ${Object.keys(this.hass.services.script).map(script => {
+                        return html` <mwc-list-item .value="${script}">script.${script}</mwc-list-item> `;
+                    })}
+      </ha-select>`;
+
+  }
+  generalConfig(){
+    return html`
+    <ha-textfield  label="displayed name(Empty for default)" id="name" .value="${this._config.name}" @input="${this.updateIt}" ></ha-textfield>
+    <ha-formfield class="switch-wrapper" label="Fancy Borders"><ha-switch id="fancy-borders-selector" .checked="${this._config.fancy_borders}" @change="${this.updateIt}"></ha-switch></ha-formfield><br>
+  `;
+  }
+
+  changeTab(e){
+    console.log(this.tabs);
+      if(e.currentTarget.classList[0].match(/^(dblclick|hold)$/))
+        Object.keys(this.tabs).filter(val => val.match(/^(dblclick|hold)$/)).forEach(va => this.tabs[va] = false);
+      else if (e.currentTarget.classList[0].match(/^(power|mute|source|touchpad|settings)$/)){
+        Object.keys(this.tabs).forEach(e => this.tabs[e] = false);
+        if(e.currentTarget.classList[0] != 'settings')
+          this.tabs['dblclick'] = true;
+      }
+      this.tabs[e.currentTarget.classList[0]] = true;
+      //console.log(this.tabs);
+      //console.log(this.hass.services.script['prova_notify']);
+      //this.hass.callService('script','prova_notify')
+      this.requestUpdate();
+    }
+
+
   updateIt(e,ha = this.hass){
-    if(e.target.id === 'entity-selector'){
-      bool = true;
+    console.log(e?.target.id);
+    if(e?.target.id === 'entity-selector'){
       this._config.entity = Object.keys(ha.states).filter(ent => ent.match('media_player[.]'))[e.detail.index] ;
     }
-    if(e.target.id === 'fancy-borders-selector' )
-      this._config.fancy_borders = !this._config.fancy_borders ;
+    if(this._config?.entity != undefined){
+    if(!this._config.hasOwnProperty('options') || !this._config.hasOwnProperty('name')){
+      this._config.name = this._config?.name?.length > 10 ? this.config.name.slice(0,10) : this._config?.name || this.hass.states[this._config?.entity].attributes.friendly_name.toUpperCase().length > 10 ? this.hass.states[this._config?.entity].attributes.friendly_name.toUpperCase().slice(0,10) : this.hass.states[this._config?.entity].attributes.friendly_name.toUpperCase();      
+      this._config.options = {
+        power: {dblclick: 'no-action',hold: 'no-action'},
+        source: {dblclick: 'no-action',hold: 'no-action'},
+        mute: {dblclick: 'no-action',hold: 'no-action'},
+        touchpad: {dblclick: 'no-action',hold: 'no-action'}
+      }
+    }
+
+      if(e?.target.id === 'hold-service-selector'){
+        if(this._config.options[Object.keys(this.tabs).filter(e => this.tabs[e] === true)[0]].hold != e.detail.index > 0 ? Object.keys(ha.services.script)[e.detail.index-1] : 'no-action')
+          this._config.options[Object.keys(this.tabs).filter(e => this.tabs[e] === true)[0]].hold = e.detail.index > 0 ? Object.keys(ha.services.script)[e.detail.index-1] : 'no-action' ;
+        else
+          return;
+      }
+      if(e?.target.id === 'dblclick-service-selector'){
+        if(this._config.options[Object.keys(this.tabs).filter(e => this.tabs[e] === true)[0]].dblclick != e.detail.index > 0 ? Object.keys(ha.services.script)[e.detail.index-1] : 'no-action')
+          this._config.options[Object.keys(this.tabs).filter(e => this.tabs[e] === true)[0]].dblclick = e.detail.index > 0 ? Object.keys(ha.services.script)[e.detail.index-1] : 'no-action' ;
+        else
+          return;
+      }
+
+      if(e?.target.id === 'name')
+        this._config.name = e?.target.value === '' ? this.hass.states[this._config?.entity].attributes.friendly_name.toUpperCase().length > 10 ? this.hass.states[this._config?.entity].attributes.friendly_name.toUpperCase().slice(0,10) : this.hass.states[this._config?.entity].attributes.friendly_name.toUpperCase() : e?.target.value;
+
+      if(e?.target.id === 'fancy-borders-selector' )
+        this._config.fancy_borders = !this._config.fancy_borders ;
+    }
+    console.log('aggiorno');
     const event = new Event("config-changed", {
       bubbles: true,
       composed: true
     });
     event.detail = {config: this._config};
     this.dispatchEvent(event); 
+    this.requestUpdate();
   }
 
   static get styles(){
@@ -457,6 +588,65 @@ class ContentCardEditor extends LitElement {
       * {
         padding: 0.5rem;
       }
+      .selected {
+        border-radius: 0.9rem 0.9rem 0 0;
+        padding: 0.5rem 2rem;
+        background-color: #242e42
+      }
+      .sub-selected {
+        color: white;
+        padding-bottom:0;
+        border-bottom: 3px solid var(--mdc-theme-primary);
+        border-top: 0;
+      }
+      .sub-tabs-row :not(.sub-selected){
+        color: gray;
+      }
+      .tab-row{
+        margin-bottom: 0px;
+        padding-bottom:0;
+        padding-left:0.6rem;
+      }
+      ha-icon {
+        color: gray;
+      }
+      .selected ha-icon {
+        color: white
+      }
+      .tab-row .tab{
+        
+        display: inline-block;
+        margin-bottom: 0px;
+      }
+      .sub-tabs-row .tab{
+        display: inline-block;
+        padding-left: 1rem;
+        padding-right: 1rem;
+      }
+      .sub-tabs-row{
+        padding-top:0;
+        padding-bottom:0;
+        text-align: center;
+        margin:0 auto auto auto;
+      }
+      .content{
+        display:block;
+      }
+      .sub-tabs {
+
+        border-top: 3px solid var(--mdc-theme-primary);
+        padding:0;
+        border-radius: 0.2rem ;
+        background-color: #242e42;
+      }
+      .hide{
+        display: none;
+      }
+      .show{
+        display: block;
+      }
+
+      
     `;
   }
 }
